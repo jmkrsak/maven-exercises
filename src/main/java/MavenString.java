ff6ace27aa1eee1f564810c80f53b21785ebb319
John-Michael Krsak <jmkrsak@gmail.com>
2022-12-05 10:44:07 -0500

    added Student and StudentTest class

--- src/main/java/MavenString.java
+++ src/main/java/MavenString.java
@@ -1,6 +1,11 @@
+import java.lang.Object;
+import org.apache.commons.lang3.StringUtils;
 public class MavenString {
 
     public static void main(String[] args) {
+
+        System.out.print("Enter input here: ");
+
 
     }
 
