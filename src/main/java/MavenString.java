faecae6b4c49e31f9fe0ce083d8dfda8d309aee6
John-Michael Krsak <jmkrsak@gmail.com>
2022-12-12 09:34:02 -0500

    update

--- src/main/java/MavenString.java
+++ src/main/java/MavenString.java
@@ -1,11 +1,26 @@
-import java.lang.Object;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class MavenString {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter input here: ");
+        String input = scanner.nextLine();
 
+        if(StringUtils.isNumeric(input)) {
+            System.out.println("You entered " + input);
+            System.out.println(input + " is a number");
+        } else {
+            System.out.println("You entered " + input);
+            System.out.println(input + " is not a number");
+        }
+
+        String string = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("swapCase example: " + StringUtils.swapCase(string));
+        System.out.println("reverse example: " + StringUtils.reverse(string));
 
     }
 
